06bdcb8a0f3ec2cfcb6bb2d89e6f50e67b1fd518
victortejeda <victorfernandotejeda@gmail.com>
2024-12-20 23:44:15 -0400

    Initial Commit

---
+++ App_Formulary/App_FormularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_FormularyApp.swift
+//  App_Formulary
+//
+//  Created by Victor Tejeda on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct App_FormularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
